ab9c42eaaef36a70fe55feebf4a226b5ba5db594
Alois Heloir <alois.heloir@live.fr>
2026-01-30 13:18:41 +0100

    chore: enable eslint type-aware rules
    Closes #247

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -176,6 +176,9 @@ export default defineNuxtConfig({
         html: true,
         markdown: 'prettier',
       },
+      typescript: {
+        tsconfigPath: 'tsconfig.json',
+      },
     },
   },
 
